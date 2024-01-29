8d69a1129fe70f4e9ea9136133fe1d7e331f5627
zaheenrakib <rakibislambest@gmail.com>
2024-01-29 23:31:10 +0600

    add some

---
+++ decremental-for.js
@@ -0,0 +1,15 @@
+//increment
+// for (let i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+//decrement
+
+//  for (let i = 10; i >=0; i--){
+//     console.log(i);
+//  }
+
+
+for (let i = 20; i >= 11; i--){
+    console.log(i);
+}
